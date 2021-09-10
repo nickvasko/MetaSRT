2a1e0e21f5b3da0d925ec28ed7bb34436bda0662
Nick Vasko <nicholas.d.vasko@gmail.com>
2021-09-10 16:34:30 -0400

    fixed apex in scripts

--- scripts/consert-joint-unsup-sts-base.sh
+++ scripts/consert-joint-unsup-sts-base.sh
@@ -1,4 +1,4 @@
 python3 main.py --continue_training --model_name_or_path ./output/sup-consert-joint-unsup-base --model_save_path ./output/sup-consert-joint-unsup-sts-base \
-  --use_apex_amp --apex_amp_opt_level 01 --force_del --seed 1 --batch_size 96 --max_seq_length 64 --evaluation_steps 200 \
+  --use_apex_amp --apex_amp_opt_level O1 --force_del --seed 1 --batch_size 96 --max_seq_length 64 --evaluation_steps 200 \
   --concatenation_sent_max_square \
   --train_data stsb --num_epochs 100 --patience 10
